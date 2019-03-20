8950f031914b06b1fd5c414d91ba1252aaeebe94
Aidan Reidel <aidan.reidel@gmail.com>
2019-03-20 11:37:13 -0700

    Added comments and fixed variable names

--- src/main.elm
+++ src/main.elm
@@ -36,7 +36,7 @@ update msg model =
 view : Model -> Html Msg
 view model = 
     div []
-    [ div [class "banner"] [h1 [] [text "Calcuweightor2"] ] 
+    [ div [class "banner"] [h1 [] [text "CalcuWeightor2"] ] 
     , div [class "main" ] 
       [ table [] 
         [ tr [] 
@@ -48,7 +48,7 @@ view model =
       ]
     ]
     
-
+{- Validates data and displays a relevant error or processes the data! -}
 viewValidation : Model -> Html msg
 viewValidation model =
   case String.toFloat( model.workWeight ) of
@@ -63,51 +63,53 @@ viewValidation model =
         div [style "color" "red" ] [ text "Work weight must be a multiple of 2.5"]
 
       else 
-        calcuweightWarmups weight
+        calcuWeight weight
 
--- TODO: Maybe refactor this 
+{- It was easier for me to check if a string that I know is a floating point number 
+  is divided by 2.5 evenly rather than do a bunch of math, and this works! -}
 twoPointFiveDivides : String -> Bool
 twoPointFiveDivides s = 
   if String.contains "." s then 
-    (String.endsWith ".5" s) && (endsWith2or7 s)
+    ( String.endsWith ".5" s ) && (endsWithOneOf ["2", "7"] ( String.dropRight 2 s ) ) 
+
   else 
-    ( String.endsWith "0" s ) || ( String.endsWith "5" s )
-  
+    endsWithOneOf ["0", "5"] s  
 
-endsWith2or7 : String -> Bool
-endsWith2or7 = 
-  (\n -> n == "2" || n == "7" ) << String.right 1 << String.dropRight 2
+{- A way of checking if a string ends eith one of a list of chars -}
+endsWithOneOf :  List String -> String -> Bool
+endsWithOneOf ls s = 
+  List.foldr (\n m -> ( String.endsWith n s ) || m ) False ls
 
-calcuweightWarmups : Float -> Html msg
-calcuweightWarmups weight = 
+{- Runs the two required calcuweightions and formats the output -}
+calcuWeight : Float -> Html msg
+calcuWeight weight = 
   div [ class "container" ] 
-  [ table [] 
-{--
-      [ tr []
-        [ td [] []
-        , td [] [ text "Weight" ]  
-        , td [] [ text "Plates" ]
-        ]
-      , 
---}
-      [ tableRow "Warm up 1:" ( warmUpWeight weight 1 )
-      , tableRow "Warm up 2:" ( warmUpWeight weight 2 )
-      , tableRow "Warm up 3:" ( warmUpWeight weight 3 )
-      , tableRow "Work sets:" weight 
+    [ table [] 
+      [ weightTableRow "Warm up 1:" ( warmUpWeight 1 weight)
+      , weightTableRow "Warm up 2:" ( warmUpWeight 2 weight)
+      , weightTableRow "Warm up 3:" ( warmUpWeight 3 weight)
+      , weightTableRow "Work sets:" weight 
       ]
-  ]
+    ]
 
-tableRow : String -> Float -> Html msg
-tableRow s w = 
+
+{- I was reusing this code block a lot so I abstracted it-}
+weightTableRow : String -> Float -> Html msg
+weightTableRow s w = 
   tr []        
-        [ td [] [ text s ]
-        , td [] [ text ( String.fromFloat w )] 
-        , td [] [ text ( plates w regularPlates )]
-        ]
+    [ td [] [ text s ]
+    , td [] [ text ( String.fromFloat w )] 
+    , td [] [ text ( plates w regularPlates )]
+    ]
 
+-- CALCUWEIGHTIONS
+
+{- The way I progress through my loaded warmups is to divide the diffrence between the empty bar and 
+  my work set weight into four parts and add one of those parts to the bar after each loaded warmup 
+  until the 4th addtion which is when its time to switch over to worksets -}
 warmUpWeight : Float -> Float -> Float
-warmUpWeight x y =
-  roundToNearest 5 ( y * ( ( x - 45 ) / 4 ) + 45 )
+warmUpWeight n w =
+  roundToNearest 5 ( n * ( ( w - 45 ) / 4 ) + 45 )
 
 {- I just think this is a neat function -}
 roundToNearest : Float -> Float -> Float
@@ -116,21 +118,27 @@ roundToNearest y x =
 
 {- This removes the bar and half of the weight as that will be mirrored in practice -}
 plates : Float -> List Float -> String
-plates x ps =
-  plateStack ( ( x - 45 ) / 2 ) ps
+plates w ps =
+  plateStack ( ( w - 45 ) / 2 ) ps
 
-{- Not allowed to pattern match on floats so we will use an if -}
+{- See if the weight remaning can have the current largest plate removed from it:
+  If yes then append that weight to the front of the string with the next call of
+  platestack
+  If not then try again with the next plate down. 
+  If no plates can be removed return the empty string. -}
 plateStack : Float -> List Float -> String 
-plateStack x ps =
+plateStack w ps =
   case ps of 
     [] -> ""
-    a::ys -> 
-      if x - a > 0 then 
-        String.fromFloat(a) ++ ", " ++ (plateStack (x-a) ps)
-      else if x - a == 0 then
-        String.fromFloat(a)
+    p::psPrime -> 
+      if w - p > 0 then 
+        String.fromFloat(w) ++ ", " ++ (plateStack (w-p) ps)
+
+      else if w - p == 0 then
+        String.fromFloat(w)
+
       else 
-        plateStack x ys
+        plateStack w psPrime
               
 regularPlates : List Float
 regularPlates = [45,35,25,10,5,2.5,1.25]
